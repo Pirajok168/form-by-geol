bf75c3c08965fa5dfb0fcb8893578873eb7609ef
Danila <danilka.eremin@gmail.com>
2022-02-03 21:24:49 +0800

    линия

--- src/main/java/MainController.java
+++ src/main/java/MainController.java
@@ -14,12 +14,15 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.control.*;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import printing.Main;
 
 public class MainController {
@@ -34,6 +37,8 @@ public class MainController {
     private MenuItem printing;
 
 
+
+
     private Model model = Single.getInstance().getModelFirstList();
     private FirstListController firstListController = new FirstListController();
     private SecondListController secondListController = new SecondListController();
@@ -41,6 +46,56 @@ public class MainController {
 
     private ObservableList<LeftTabs> leftTabsList = FXCollections.observableArrayList(Arrays.asList(LeftTabs.Documentation
             , LeftTabs.Result, LeftTabs.Table, LeftTabs.LastTable, LeftTabs.Akt ));
+
+
+    @FXML
+    private TextField borehole;
+
+    @FXML
+    private Button btnAddBorehole;
+
+    @FXML
+    private Button btnAddLine;
+
+    @FXML
+    private TextField line;
+
+    @FXML
+    private Accordion listLines;
+
+
+    @FXML
+    void addLine(ActionEvent event) {
+        ObservableList<String> books = FXCollections.observableArrayList();
+        VBox content = new VBox();
+        HBox content2 = new HBox();
+        Button button = new Button("Добавить скважину");
+        ListView<String> listBorehole = new ListView<>();
+        listBorehole.minHeight(400);
+        listBorehole.setItems(books);
+        TextField textField = new TextField();
+
+        button.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                books.add(textField.getText());
+            }
+        });
+
+        textField.setPromptText("Введите номер скважины");
+
+        content2.setAlignment(Pos.CENTER);
+        content2.getChildren().addAll(textField, button);
+        content.getChildren().addAll(content2, listBorehole);
+
+
+        TitledPane titledPane = new TitledPane();
+        titledPane.setText(line.getText());
+        titledPane.minHeight(400);
+        titledPane.setContent(content);
+        listLines.getPanes().add(titledPane);
+    }
+
 
 
 
